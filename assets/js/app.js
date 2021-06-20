d05caac49810a93ec6bf54dd440b219da2f68f4d
Sachin Singh <sachin.developer.1994@gmail.com>
2021-06-20 13:20:32 +0530

    Responsive Menu toggling and Related CSS created Commit-No:5

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,9 @@
 let navButton = document.getElementById('navButton');
 let navBlock = document.getElementById('navBlock');
 let closeMenuButton = document.getElementById('closeMenu');
+let submenu1 = document.getElementsByClassName('submenu1');
+let submenu2 = document.getElementsByClassName('submenu2');
+
 
 navButton.addEventListener('click',showMenu);
 closeMenuButton.addEventListener('click',closeMenu)
@@ -13,4 +16,5 @@ function showMenu(){
 function closeMenu(){
     document.body.style.background ='';
     navBlock.style.transform='translateX(-100%)';
+    submenu1.background='red';
 }
